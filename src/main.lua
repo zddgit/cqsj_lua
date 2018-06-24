a968642b7ad9c470bd8bc501ab7d96387ef8e512
zdd <583627140@qq.com>
2018-06-24 18:08:13 +0800

    增添识别所在地图的实现

--- src/main.lua
+++ src/main.lua
@@ -2,20 +2,27 @@ cqsj = require("cqsj");
 cqsj:init();
 mSleep(3000);
 local rect = {1145,31,1272,54}
-local diff  = {"0xCB9672-0x20281A"}
-local whitelist  = "中州神威狱圣殿走廊";
+local diff  = {"0xC99874-0x2C3321"}
+local whitelist  = "中州神威狱圣殿走廊猛兽高原魔宫太上玉清秘境全民宝地五层";
 local code,text = cqsj.gamehelper.readScreen(rect,diff,whitelist,"[external]","chi_sim")
 sysLog("code:"..code);
 sysLog("text:"..text);
-if text == "神威狱" then
+if string.find (text, "神威狱") ~= nil then
 	cqsj:quick_task(cqsj.swyOne);
 end
-sysLog(string.len(text))
-sysLog(string.len("中州"))
-if string.find (text, "中州") ~= nil then
-	sysLog("人物在中州");
+if string.find (text, "高原") ~= nil then
+	cqsj:quick_task(cqsj.msgy);
 end
---cqsj:zhaoling_task();
---cqsj:gold_shoumo();
+if string.find (text, "圣殿魔宫") ~= nil then
+	cqsj:quick_task(cqsj.sdmg);
+end
+if string.find (text, "秘境") ~= nil then
+	cqsj:quick_task(cqsj.mj);
+end
+if string.find(text,"全民") ~=nil and  string.find(text,"五") then
+	cqsj:quick_task(cqsj.bd_five);
+end
+cqsj:zhaoling_task();
+cqsj:gold_shoumo();
 
 
