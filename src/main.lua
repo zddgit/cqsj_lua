7d906281c8750d6e3e0b28f82e4b45ab1acc4dfd
zdd <583627140@qq.com>
2018-06-29 09:19:12 +0800

    添加神威狱二层坐标

--- src/main.lua
+++ src/main.lua
@@ -3,12 +3,15 @@ cqsj:init();
 mSleep(3000);
 local rect = {1145,31,1272,54}
 local diff  = {"0xC99874-0x2C3321"}
-local whitelist  = "中州神威狱圣殿走廊猛兽高原魔宫太上玉清秘境全民宝地五层";
+local whitelist  = "中州神威狱圣殿走廊猛兽高原魔宫太上玉清秘境全民宝地一二三四五层";
 local code,text = cqsj.gamehelper.readScreen(rect,diff,whitelist,"[external]","chi_sim")
 sysLog("code:"..code);
 sysLog("text:"..text);
-if string.find (text, "神威狱") ~= nil then
+if string.find (text, "神威狱") ~= nil and string.find (text, "一")  then
 	cqsj:quick_task(cqsj.swyOne);
+end
+if string.find (text, "神威狱") ~= nil and string.find (text, "二")  then
+	cqsj:quick_task(cqsj.swyTwo);
 end
 if string.find (text, "高原") ~= nil then
 	cqsj:quick_task(cqsj.msgy);
